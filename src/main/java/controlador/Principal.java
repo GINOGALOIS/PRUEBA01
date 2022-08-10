00c5504828db96511adf417b88a54225f927adc3
GINOGALOIS <020101702H@uandina.edu.pe>
2022-08-10 10:21:52 -0500

    Initial commit

---
+++ src/main/java/controlador/Principal.java
@@ -0,0 +1,28 @@
+package controlador;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Principal extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Principal.class.getResource("/Vista/Vista01.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+    @Override
+    public void stop(){
+        System.out.println("Fin del programa () ");}
+
+
+}
